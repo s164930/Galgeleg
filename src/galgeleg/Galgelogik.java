8755b8804f3b9a6138d3e46a2fb26ddc565a7c8a
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2017-10-24 13:31:10 +0200

    Mindre forbedringer

--- src/galgeleg/Galgelogik.java
+++ src/galgeleg/Galgelogik.java
@@ -62,6 +62,7 @@ public class Galgelogik {
     muligeOrd.add("gangsti");
     muligeOrd.add("skovsnegl");
     muligeOrd.add("solsort");
+    muligeOrd.add("solsort");
     nulstil();
   }
 
@@ -125,6 +126,7 @@ public class Galgelogik {
 
 
   public static String hentUrl(String url) throws IOException {
+    System.out.println("Henter data fra " + url);
     BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
     StringBuilder sb = new StringBuilder();
     String linje = br.readLine();
@@ -142,11 +144,18 @@ public class Galgelogik {
 
     data = data.substring(data.indexOf("<body")). // fjern headere
             replaceAll("<.+?>", " ").toLowerCase(). // fjern tags
+            replaceAll("&#198;", "æ"). // erstat HTML-tegn
+            replaceAll("&#230;", "æ"). // erstat HTML-tegn
+            replaceAll("&#216;", "ø"). // erstat HTML-tegn
+            replaceAll("&#248;", "ø"). // erstat HTML-tegn
+            replaceAll("&oslash;", "ø"). // erstat HTML-tegn
+            replaceAll("&#229;", "å"). // erstat HTML-tegn
             replaceAll("[^a-zæøå]", " "). // fjern tegn der ikke er bogstaver
             replaceAll(" [a-zæøå] "," "). // fjern 1-bogstavsord
             replaceAll(" [a-zæøå][a-zæøå] "," "); // fjern 2-bogstavsord
 
     System.out.println("data = " + data);
+    System.out.println("data = " + Arrays.asList(data.split("\\s+")));
     muligeOrd.clear();
     muligeOrd.addAll(new HashSet<String>(Arrays.asList(data.split(" "))));
 
